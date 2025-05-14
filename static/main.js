86562f52856c28fecd43c0e9fe680fe67365a584
superhenryman <121818148+superhenryman@users.noreply.github.com>
2025-05-14 16:55:45 +0300

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,21 +1,23 @@
 const form = document.getElementById('form');
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
-            
+
     const formdata = new FormData(form);
     const response = await fetch("/compile", {
-        method:"post",
+        method: "post",
         body: formdata
     });
-    const fileBlob = await response.blob();
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(fileBlob);
-    link.download = "compiled.exe";
-    link.click(); 
-    link.style.display = 'none';
-    if (response.ok) {
-        fetch("/delete")
+
+    if (response.ok && response.headers.get('Content-Disposition')) {
+        const fileBlob = await response.blob();
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(fileBlob);
+        link.download = "compiled.exe";
+        link.click();
+        link.style.display = 'none';
+        fetch("/delete"); // No need to check response here if download was successful
     } else {
-        alert("Error while deleting file on server")
+        const errorData = await response.json();
+        alert("An unexpected error occurred during compilation." + errorData);
     }
 });
